ba6971cf655a117a6d8d21748948b199d07bfeb7
Your Name <you@example.com>
2023-05-18 22:52:18 +0900

    add run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+sudo apt install git curl python3 python3-pip  build-essential vim ripgrep universal-ctags zsh -y 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
