098e03e76c0a782ec716276fe4a4b75d82402679
JJY-sec <64367280+JJY-sec@users.noreply.github.com>
2024-07-05 09:03:25 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-sudo apt install git curl python3 python3-pip  build-essential vim ripgrep universal-ctags zsh -y 
+sudo sed -i 's/archive.ubuntu.com/mirror.kakao.com/g' /etc/apt/sources.list
+sudo apt update
+sudo apt install git curl python3 python3-pip  build-essential neovim ripgrep universal-ctags zsh -y  
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
